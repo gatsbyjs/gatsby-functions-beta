b63ae3d1a4e1fc77d99b9e3a2250c07c6e59ec32
polina <polina@polinas-MacBook-Pro.local>
2021-04-19 15:55:46 -0700

    gatsby config update.

--- examples/twilio-text/gatsby-config.js
+++ examples/twilio-text/gatsby-config.js
@@ -1,9 +1,11 @@
 module.exports = {
+  flags: {
+    FUNCTIONS: true,
+  },
   siteMetadata: {
     title: "Twilio Text",
   },
   plugins: [
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-functions"
+    "gatsby-plugin-gatsby-cloud"
   ],
 };
