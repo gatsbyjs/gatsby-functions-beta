3972dfe3d3221268bc8a524878e5df8188a9bb9f
Joel Smith <jsumnersmith@gmail.com>
2021-04-30 11:38:46 -0400

    Removed all Gatsby page styling and content. Now just a README repo with the examples.

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: "Functions Starters",
-    repo: "https://github.com/gatsbyjs/functions-starters"
-  },
-  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-gatsby-cloud"],
-};
